6faa552312f58b2ca3ea31d8c80a9b13cd068529
shastaxc <shastaxc@gmail.com>
2020-12-30 12:40:51 -0500

    Add ref

---
+++ references/Thesteja-RNG.lua
@@ -0,0 +1,6 @@
+-- Initialization function for this job file.
+function get_sets()
+  -- Load and initialize Mote library
+  mote_include_version = 2
+  include('Mote-Include.lua') -- Executes job_setup, user_setup, init_gear_sets
+end
