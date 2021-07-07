99191cfa34a973632f918e4649be93e4f4ab173e
shastaxc <shastaxc@gmail.com>
2021-07-07 16:34:45 -0400

    Remove unused gear

--- Kinkima-Augments.lua
+++ Kinkima-Augments.lua
@@ -80,7 +80,6 @@ gear.Merl_FC_feet = { name="Merlinic Crackows", augments={'Accuracy+6','INT+10',
 
 gear.Merl_MB_body = { name="Merlinic Jubbah", augments={'Pet: STR+10','DEX+8','Magic burst dmg.+7%','Accuracy+20 Attack+20',}} --10 MB Dmg, 40 MAB, 40 M.Acc, 10 INT
 gear.Merl_MB_hands = { name="Merlinic Dastanas", augments={'Accuracy+10 Attack+10','DEX+8','Magic burst dmg.+9%','Mag. Acc.+9 "Mag.Atk.Bns."+9',}}
-gear.Merl_MB_legs = {} -- MAcc, MAB, MB Dmg
 gear.Merl_MB_feet = { name="Merlinic Crackows", augments={'Magic burst dmg.+8%','Phys. dmg. taken -2%','INT+3 MND+3 CHR+3','Accuracy+7 Attack+7','Mag. Acc.+1 "Mag.Atk.Bns."+1',}}--Max: 10 MB Dmg, 40 MAB, 40 M.Acc, 10 INT
 
 -- Odyssean
