26bc993938ca412fc99c0e43c142680aa12d4081
shastaxc <shastaxc@gmail.com>
2021-08-17 08:54:13 -0400

    Fix formatting

--- Silvermutt-COR.lua
+++ Silvermutt-COR.lua
@@ -1002,7 +1002,7 @@ function init_gear_sets()
     head=gear.Adhemar_B_head,
     body=gear.Adhemar_B_body,
     hands=gear.Adhemar_B_hands,
-    legs="Samnuha Tights",
+    legs=gear.Samnuha_legs,
     feet=gear.Herc_TA_feet,
     neck="Iskur Gorget",
     ear1="Cessance Earring",
@@ -1111,7 +1111,7 @@ function init_gear_sets()
     head=gear.Adhemar_B_head,
     body=gear.Adhemar_B_body, --6
     hands=gear.Adhemar_B_hands,
-    legs="Samnuha Tights",
+    legs=gear.Samnuha_legs,
     feet=gear.Taeon_DW_feet, --9
     neck="Iskur Gorget",
     ear1="Suppanomimi", --5
@@ -1147,7 +1147,7 @@ function init_gear_sets()
     head=gear.Adhemar_B_head,
     body=gear.Adhemar_B_body, --6
     hands=gear.Adhemar_B_hands,
-    legs="Samnuha Tights",
+    legs=gear.Samnuha_legs,
     feet=gear.Herc_TA_feet,
     neck="Iskur Gorget",
     ear1="Suppanomimi", --5
@@ -1183,7 +1183,7 @@ function init_gear_sets()
     head=gear.Adhemar_B_head,
     body=gear.Adhemar_B_body, --6
     hands=gear.Adhemar_B_hands,
-    legs="Samnuha Tights",
+    legs=gear.Samnuha_legs,
     feet=gear.Herc_TA_feet,
     neck="Iskur Gorget",
     ear1="Suppanomimi", --5
@@ -1220,7 +1220,7 @@ function init_gear_sets()
     head=gear.Adhemar_B_head,
     body=gear.Adhemar_B_body, --6
     hands=gear.Adhemar_B_hands,
-    legs="Samnuha Tights",
+    legs=gear.Samnuha_legs,
     feet=gear.Herc_TA_feet,
     neck="Iskur Gorget",
     ear1="Brutal Earring",
