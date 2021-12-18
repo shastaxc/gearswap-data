35029e356365b86e88e6070288fd3644601577cb
shastaxc <shastaxc@gmail.com>
2021-12-18 11:40:54 -0500

    Use Herc MAB feet for trueflight

--- Silvermutt-RNG.lua
+++ Silvermutt-RNG.lua
@@ -553,7 +553,7 @@ function init_gear_sets()
     body=gear.Nyame_B_body,
     hands=gear.Nyame_B_hands,
     legs=gear.Nyame_B_legs,
-    feet=gear.Nyame_B_feet,
+    feet=gear.Herc_MAB_feet,
     neck="Scout's Gorget +1",
     ear1="Friomisi Earring", --10
     ear2="Moonshade Earring",
