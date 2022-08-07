752955b7d1463ca16b73549491eea84fe3211550
Dakota Jordan <shastaxc@gmail.com>
2022-08-07 09:43:47 -0500

    Change keybinds for cureplease and fast follow

--- Global-Binds.lua
+++ Global-Binds.lua
@@ -28,9 +28,9 @@ send_command('bind @numpad1 input /ma "Paralyna" <stpc>')
 send_command('bind @numpad2 input /ma "Blindna" <stpc>')
 send_command('bind @numpad3 input /ma "Silena" <stpc>')
 
-send_command('bind scrolllock input //ffo me; wait 0.1; input //cureplease stop')
-send_command('bind pause input //ffo stopall; wait 0.1; input //cureplease start')
-send_command('bind !pause input //ffo stopall; wait 0.1; input //cureplease stop')
+send_command('bind !Numpad/ input //ffo me; wait 0.3; input //cureplease stop')
+send_command('bind !Numpad* input //ffo stopall; wait 0.3; input //cureplease start')
+send_command('bind !Numpad- input //ffo stopall; wait 0.3; input //cureplease stop')
 
 send_command('bind !j gs c invisible')
 send_command('bind !k gs c sneak')
