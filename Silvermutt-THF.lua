6793b7ad10070f890cb63c005aaad62b665d5e98
shastaxc <shastaxc@gmail.com>
2022-12-11 21:32:08 -0500

    Fix unbinds

--- Silvermutt-THF.lua
+++ Silvermutt-THF.lua
@@ -233,9 +233,9 @@ function job_file_unload()
   send_command('unbind !`')
   send_command('unbind @c')
 
-  send_command('bind ^numpad0 input /ja "Sneak Attack" <me>')
-  send_command('bind ^numpad. input /ja "Trick Attack" <me>')
-  send_command('bind %e input /ra <t>')
+  send_command('unbind ^numpad0')
+  send_command('unbind ^numpad.')
+  send_command('unbind %e')
 
   send_command('unbind !w')
   send_command('unbind ^numpad/')
