8e847e81ae94228271a001df43f3d1b6048a5ede
shastaxc <shastaxc@gmail.com>
2023-05-24 00:38:17 -0400

    Repurpose merlinic body

--- Kinkima-Augments.lua
+++ Kinkima-Augments.lua
@@ -69,7 +69,6 @@ gear.Merl_FC_body = { name="Merlinic Jubbah", augments={'Mag. Acc.+2 "Mag.Atk.Bn
 gear.Merl_FC_legs = { name="Merlinic Shalwar", augments={'Accuracy+8','"Fast Cast"+7','Mag. Acc.+15',}}
 gear.Merl_FC_feet = { name="Merlinic Crackows", augments={'Mag. Acc.+8','"Fast Cast"+7','CHR+2',}}
 
-gear.Merl_MB_body = { name="Merlinic Jubbah", augments={'INT+3','Magic burst dmg.+11%','Accuracy+18 Attack+18',}} --10 MB Dmg, 40 MAB, 40 M.Acc, 10 INT
 gear.Merl_MB_hands = { name="Merlinic Dastanas", augments={'Accuracy+10 Attack+10','DEX+8','Magic burst dmg.+9%','Mag. Acc.+9 "Mag.Atk.Bns."+9',}}
 gear.Merl_MB_feet = { name="Merlinic Crackows", augments={'Magic burst dmg.+8%','Phys. dmg. taken -2%','INT+3 MND+3 CHR+3','Accuracy+7 Attack+7','Mag. Acc.+1 "Mag.Atk.Bns."+1',}}--Max: 10 MB Dmg, 40 MAB, 40 M.Acc, 10 INT
 
