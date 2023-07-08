404ac45bcba32259d28dd5fc4905b6b6e09d945b
shastaxc <shastaxc@gmail.com>
2023-07-07 20:34:43 -0400

    Add empy earring

--- Kinkima-GEO.lua
+++ Kinkima-GEO.lua
@@ -1091,12 +1091,12 @@ function init_gear_sets()
     feet="Azimuth Gaiters +2",        -- __, __, __ [10/10, 158] {__/__, __}
     neck="Loricate Torque +1",        -- __, __, __ [ 6/ 6, ___] {__/__, __}
     ear1="Mimir Earring",             -- __, __, __ [__/__, ___] {__/__, __}
-    ear2="Hearty Earring",            -- __, __, __ [__/__, ___] {__/__, __}
+    ear2="Azimuth Earring +1",        -- __, __, __ [ 3/ 3, ___] {__/__, __}
     ring1="Gelatinous Ring +1",       -- __, __, __ [ 7/-1, ___] {__/__, __}
     ring2="Defending Ring",           -- __, __, __ [10/10, ___] {__/__, __}
     back=gear.GEO_Idle_Cape,          -- __, __, __ [__/__,  30] {__/__, 15}
     waist="Embla Sash",               -- __, __, 10 [__/__, ___] {__/__, __}
-    -- 2 Refresh Potency, 0 Refresh Duration, 46% Enh Duration [47 PDT/39 MDT, 598 M.Eva] {Pet: 0 PDT/0 MDT, 15 Regen}
+    -- 2 Refresh Potency, 0 Refresh Duration, 46% Enh Duration [50 PDT/42 MDT, 598 M.Eva] {Pet: 0 PDT/0 MDT, 15 Regen}
     
     -- body=gear.Telchine_ENH_body,   -- __, __, 10 [__/__, 105] {__/__, __}
     -- hands="Azimuth Gloves +3",     -- __, __, __ [12/12,  98] {__/__, __}
@@ -1124,13 +1124,14 @@ function init_gear_sets()
     legs=gear.Nyame_B_legs,           -- __, __ [ 8/ 8, 150] {__/__, __}
     feet="Azimuth Gaiters +2",        -- __, __ [10/10, 158] {__/__, __}
     neck="Loricate Torque +1",        -- __, __ [ 6/ 6, ___] {__/__, __}
-    ear2="Hearty Earring",            -- __, __ [__/__, ___] {__/__, __}
+    ear1="Hearty Earring",            -- __, __ [__/__, ___] {__/__, __}
+    ear2="Azimuth Earring +1",        -- __, __ [ 3/ 3, ___] {__/__, __}
     ring1="Gelatinous Ring +1",       -- __, __ [ 7/-1, ___] {__/__, __}
     ring2="Defending Ring",           -- __, __ [10/10, ___] {__/__, __}
     back=gear.GEO_FC_Cape,            -- __, __ [10/__, ___] {__/__, __}
     waist="Emphatikos Rope",          --  1, __ [__/__, ___] {__/__, __}
     -- Base                               1
-    -- 6 Aquaveil, 20% Enh Duration [51 PDT/33 MDT, 551 M.Eva] {Pet: 30 PDT/30 MDT, 0 Regen}
+    -- 6 Aquaveil, 20% Enh Duration [54 PDT/36 MDT, 551 M.Eva] {Pet: 30 PDT/30 MDT, 0 Regen}
 
     -- main="Vadose Rod",             --  1, __ [__/__, ___] {__/__, __}
     -- body=gear.Telchine_ENH_body,   -- __, 10 [__/__, 105] {__/__, __}
@@ -1243,17 +1244,17 @@ function init_gear_sets()
     feet="Bagua Sandals +3",        -- __ [__/__, 127] {__/__,  5}
     neck="Bagua Charm +1",          -- __ [__/__, ___] {__/__, __}; Absorb Dmg+8
     ear1="Arete Del Luna +1",       -- __ [__/__, ___] {__/__, __}; Resists
-    ear2="Hearty Earring",          -- __ [__/__, ___] {__/__, __}; Status Resist+5
+    ear2="Azimuth Earring +1",      -- __ [ 3/ 3, ___] {__/__, __}
     ring1="Stikini Ring +1",        --  1 [__/__, ___] {__/__, __}
     ring2="Defending Ring",         -- __ [10/10, ___] {__/__, __}
     back=gear.GEO_Idle_Cape,        -- __ [__/__,  30] {__/__, 15}
     waist="Isa Belt",               -- __ [__/__, ___] { 3/ 3,  1}; Prefer refresh if it existed
-    -- 6 Refresh [47 PDT/24 MDT, 553 M.Eva] {Pet: 41 PDT/41 MDT, 25 Regen}
+    -- 6 Refresh [50 PDT/27 MDT, 553 M.Eva] {Pet: 41 PDT/41 MDT, 25 Regen}
 
     -- head="Azimuth Hood +3",      -- __ [12/12, 136] {__/__,  5}
     -- neck="Bagua Charm +2",       -- __ [__/__, ___] {__/__, __}; Absorb Dmg+10
-    -- ear2="Genmei Earring",       -- __ [ 2/__, ___] {__/__, __}
-    -- 6 Refresh [50 PDT/25 MDT, 563 M.Eva] {Pet: 41 PDT/41 MDT, 26 Regen}
+    -- ear2="Azimuth Earring +2",   -- __ [ 7/ 7, ___] {__/__, __}
+    -- 6 Refresh [55 PDT/32 MDT, 563 M.Eva] {Pet: 41 PDT/41 MDT, 26 Regen}
   }
 
   sets.idle.HeavyDef.Pet = set_combine(sets.idle.Pet, {})
