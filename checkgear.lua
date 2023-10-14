efa67a704c5e792fdf09e5cb6c53491c207a22db
shastaxc <shastaxc@gmail.com>
2023-10-13 21:54:58 -0400

    Update checkgear script

--- checkgear.lua
+++ checkgear.lua
@@ -1,17 +1,19 @@
 -- Initialization function for this job file.
- 
-local jobs = T{"BLU", "BRD", "COR", "DNC", "DRG", "GEO", "MNK", "NIN", "RDM", "RNG", "RUN", "SAM", "THF", "WAR"}
- 
+
+local jobs = T{'BLU', 'BRD', 'BST', 'COR', 'DNC', 'DRG', 'GEO', 'MNK', 'NIN', 'PUP', 'RNG', 'RUN', 'SAM', 'THF', 'WAR'}
+-- local jobs = T{'BLM', 'BLU', 'COR', 'GEO', 'RDM', 'SCH', 'SMN', 'THF', 'WHM'}
+
 mysets = {}
- 
+
 for k,v in pairs(jobs) do
-    include('Silvermutt-'..v .. '.lua')
-    get_sets()
-    mysets[v] = sets
-    sets = {}
- end
- 
- 
+  include('Silvermutt-'..v .. '.lua')
+  -- include('Kinkima-'..v .. '.lua')
+  get_sets()
+  mysets[v] = sets
+  sets = {}
+end
+
+
 function get_sets()
-    for k,v in pairs(mysets) do sets[k] = v end
+  for k,v in pairs(mysets) do sets[k] = v end
 end
