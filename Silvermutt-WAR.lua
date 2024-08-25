2f03f502dcecba930f9a34d34ab6df6f96e641cd
shastaxc <shastaxc@gmail.com>
2024-08-25 16:10:29 -0400

    Update weapon handling

--- Silvermutt-WAR.lua
+++ Silvermutt-WAR.lua
@@ -160,8 +160,7 @@ function get_sets()
   -- Load and initialize Mote library
   mote_include_version = 2
   include('Mote-Include.lua') -- Executes job_setup, user_setup, init_gear_sets
-  equip({main=empty,sub=empty})
-  
+
   coroutine.schedule(function()
     send_command('gs reorg')
   end, 1)
@@ -170,7 +169,7 @@ function get_sets()
   end, 3)
   coroutine.schedule(function()
     send_command('gs c weaponset current')
-  end, 5)
+  end, 4)
 end
 
 -- Executes on first load and main job change
@@ -261,6 +260,11 @@ function user_setup()
 
   select_default_macro_book()
   set_sub_keybinds:schedule(2)
+
+  if initialized then
+    send_command:schedule(1, 'gs c equipweapons')
+  end
+  initialized = true -- DO NOT MODIFY
 end
 
 function job_file_unload()
@@ -1249,6 +1253,8 @@ function init_gear_sets()
     ring2="Eshmun's Ring", --20
     waist="Gishdubar Sash", --10
   }
+
+  sets.FallbackShield = {sub="Blurred Shield +1"}
 end
 
 -------------------------------------------------------------------------------------------------------------------
@@ -1497,44 +1503,29 @@ function display_current_job_state(eventArgs)
   eventArgs.handled = true
 end
 
-function select_weapons()
-  if state.ToyWeapons.current ~= 'None' then
-    return sets.ToyWeapon[state.ToyWeapons.current]
-  else
-    if sets.WeaponSet[state.WeaponSet.current] then
-      if use_dw_if_available and silibs.can_dual_wield() and sets.WeaponSet[state.WeaponSet.current].DW then
-        return sets.WeaponSet[state.WeaponSet.current].DW
-      else
-        return sets.WeaponSet[state.WeaponSet.current]
-      end
-    end
-  end
-end
-
-function cycle_weapons(cycle_dir)
+function cycle_weapons(cycle_dir, set_name)
   if cycle_dir == 'forward' then
     state.WeaponSet:cycle()
   elseif cycle_dir == 'back' then
     state.WeaponSet:cycleback()
-  elseif cycle_dir == 'reset' then
+  elseif cycle_dir == 'set' then
+    state.WeaponSet:set(set_name)
+  else
     state.WeaponSet:reset()
   end
+  state.ToyWeapons:reset()
 
   add_to_chat(141, 'Weapon Set to '..string.char(31,1)..state.WeaponSet.current)
   equip(select_weapons())
 end
 
-function cycle_toy_weapons(cycle_dir)
-  --If current state is None, save current weapons to switch back later
-  if state.ToyWeapons.current == 'None' then
-    sets.ToyWeapon.None.main = player.equipment.main
-    sets.ToyWeapon.None.sub = player.equipment.sub
-  end
-
+function cycle_toy_weapons(cycle_dir, set_name)
   if cycle_dir == 'forward' then
     state.ToyWeapons:cycle()
   elseif cycle_dir == 'back' then
     state.ToyWeapons:cycleback()
+  elseif cycle_dir == 'set' then
+    state.ToyWeapons:set(set_name)
   else
     state.ToyWeapons:reset()
   end
@@ -1545,6 +1536,28 @@ function cycle_toy_weapons(cycle_dir)
   end
   add_to_chat(012, 'Toy Weapon Mode: '..string.char(31,mode_color)..state.ToyWeapons.current)
   equip(select_weapons())
+end
+
+function select_weapons()
+  local weapons_to_equip = {}
+  local can_dw = silibs.can_dual_wield()
+  if state.ToyWeapons.current ~= 'None' then
+    weapons_to_equip = set_combine(sets.ToyWeapon[state.ToyWeapons.current], {})
+  else
+    if use_dw_if_available and can_dw and sets.WeaponSet[state.WeaponSet.current] and sets.WeaponSet[state.WeaponSet.current].DW then
+      weapons_to_equip = set_combine(sets.WeaponSet[state.WeaponSet.current].DW, {})
+    elseif sets.WeaponSet[state.WeaponSet.current] then
+      weapons_to_equip = set_combine(sets.WeaponSet[state.WeaponSet.current], {})
+    end
+  end
+
+  -- If trying to equip weapon in offhand but cannot DW, equip empty
+  if not can_dw and weapons_to_equip.sub and silibs.is_weapon(weapons_to_equip.sub) then
+    local sub_to_use = sets.FallbackShield and sets.FallbackShield.sub or 'empty'
+    weapons_to_equip = set_combine(weapons_to_equip, {sub=sub_to_use})
+  end
+
+  return weapons_to_equip
 end
 
 function get_custom_wsmode(spell, action, spellMap)
@@ -1728,23 +1741,29 @@ function job_self_command(cmdParams, eventArgs)
   silibs.self_command(cmdParams, eventArgs)
   ----------- Non-silibs content goes below this line -----------
 
-  if cmdParams[1] == 'toyweapon' then
-    if cmdParams[2] == 'cycle' then
-      cycle_toy_weapons('forward')
-    elseif cmdParams[2] == 'cycleback' then
-      cycle_toy_weapons('back')
-    elseif cmdParams[2] == 'reset' then
-      cycle_toy_weapons('reset')
-    end
+  if cmdParams[1] == 'equipweapons' then
+    equip(select_weapons())
   elseif cmdParams[1] == 'weaponset' then
     if cmdParams[2] == 'cycle' then
       cycle_weapons('forward')
     elseif cmdParams[2] == 'cycleback' then
       cycle_weapons('back')
-    elseif cmdParams[2] == 'reset' then
-      cycle_weapons('reset')
     elseif cmdParams[2] == 'current' then
       cycle_weapons('current')
+    elseif cmdParams[2] == 'set' and cmdParams[3] then
+      cycle_weapons('set', cmdParams[3])
+    elseif cmdParams[2] == 'reset' then
+      cycle_weapons('reset')
+    end
+  elseif cmdParams[1] == 'toyweapon' then
+    if cmdParams[2] == 'cycle' then
+      cycle_toy_weapons('forward')
+    elseif cmdParams[2] == 'cycleback' then
+      cycle_toy_weapons('back')
+    elseif cmdParams[2] == 'set' and cmdParams[3] then
+      cycle_toy_weapons('set', cmdParams[3])
+    elseif cmdParams[2] == 'reset' then
+      cycle_toy_weapons('reset')
     end
   elseif cmdParams[1] == 'bind' then
     set_main_keybinds:schedule(2)
